0869df00a6828655bd4a3953a3f279dda2cb303e
DaltonHart <daltonvaren@gmail.com>
2022-03-18 13:01:41 -0700

    :recycle: (client) adds react client

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 /* === External Modules === */
 const express = require("express");
+const path = require("path");
 
 /* === Internal Modules === */
 
 /* === Instanced Modules === */
 const app = express();
 
-
 /* === Middleware === */
-// body parsing middle 
-// app.use(express.json()); // hey express. acccept json as body data
+// body parsing middle
+app.use(express.json());
+// server public directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // custom url logger
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
   console.log(req.url);
   next();
-})
-
+});
 
 /* === Routes === */
 
@@ -36,52 +37,14 @@ app.use(function(req,res,next){
       - DELETE
 */
 
-// .get("url", callback)
-app.get("/", function(req,res){
-  console.log({
-    body: req.body,
-    params: req.params,
-    query: req.query
-  });
-
-  res.json({message: "Hello World"});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-const cats = [
-  {
-    name: "Elenore"
-  },
-  {
-    name: "Eldritch"
-  }
-];
-
-app.get("/cats", function(req,res){
-  res.json({
-    data: cats
-  });
-});
-
-app.get("/cats/:index", function(req,res){
-  console.log(req.params);
-  res.json({
-    data: cats[req.params.index]
-  });
-});
-
-// echo 
-// callback -> controller -> request handler -> view function
-app.post("/echo", function(req,res){
-  res.json({
-    data: req.body
-  })
-})
-
 
 /* === Server Bind === */
 //Bind our server to a port
 
 // .listen(number, ?callback)
-app.listen(4000, function(){
-  console.log("Im a little server hear my port");
+app.listen(4000, function () {
+  console.log("Let's get this music goin~");
 });
